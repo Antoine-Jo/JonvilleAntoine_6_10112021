90bc789c1ba8f98bb5224971e35f93f168f44699
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-11-10 15:55:00 +0100

    Update app.js with headers

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,13 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Header qui permet d'accéder à notre API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); //Ajouter les headers mentionnés aux requêtes envoyées vers notre API
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // d'envoyer des requêtes avec les méthodes mentionnées
+    next();
+});
+
+app.use((req, res, next) => {
     console.log('Requête reçue !');
     next();
 });
